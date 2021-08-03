508c9ba9704dd6b80aaf4f7b64b67afd7a12489f
ethcala <81577564+ethcala@users.noreply.github.com>
2021-08-02 19:59:47 -0600

    added basic console interface

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#This is the main file
+path = input('Please enter path to files:\n')
+
+sel = ''
+while sel != '0':
+    print('\n1 - View file contents')
+    print('2 - View employees from files')
+    print('0 - Exit')
+
+    sel = input('Select an option: ')
+    if sel == '1':
+        print('1 selected')
+        # print_people_details(path)
+    elif sel == '2':
+        print('2 selected')
+        # print_employees(path)
+    elif sel == '0':
+        print('Closing program.')
+    else:
+        print('Instructions unclear.')
