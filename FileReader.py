0a4de0ac62686cbd4b64f9737e0fd8a96868ecd3
JRosadoDiaz <jose.rosado853@gmail.com>
2021-08-02 21:05:26 -0600

    Phase 1 of assignment completed
    + Moved the code in main.py to a main method
    + FileReader.py has been altered to fit assignment requirements

--- FileReader.py
+++ FileReader.py
@@ -1,11 +1,17 @@
 from Employee import Employee
 
-employees = [
-    #Employee(101, "Jose", "Rosado", 1996)
-]
-
+employees = []
 
 def PrintPeopleDetails(filename):
+    f = open(filename, "r")
+    print(f.read())
+
+def PrintEmployees(filename):
+    read_file(filename)
+    for x in employees:
+        print("ID: " + x.get_employee_id() + ", Name: " + x.get_employee_first_name() + " " + x.get_employee_last_name() + ", Hire Year: " + x.get_employee_hire_year())    
+
+def read_file(filename):
     f = open(filename,"r")
     lines = f.readlines()
     for x in lines:
@@ -13,8 +19,3 @@ def PrintPeopleDetails(filename):
         #print(emp)
         newEmployee = Employee(emp[0], emp[1], emp[2], emp[3])
         employees.append(newEmployee)
-
-def PrintEmployees():
-    for x in employees:
-        print(x.get_employee_first_name(), x.get_employee_hire_year())
-    
--- main.py
+++ main.py
@@ -1,30 +1,33 @@
-path = input('Please enter path to files:\n')
-
-sel = ''
-while sel != '0':
-    print('\n1 - View file contents')
-    print('2 - View employees from files')
-    print('0 - Exit')
-
-    sel = input('Select an option: ')
-    if sel == '1':
-        print('1 selected')
-        # print_people_details(path)
-    elif sel == '2':
-        print('2 selected')
-        # print_employees(path)
-    elif sel == '0':
-        print('Closing program.')
-    else:
-        print('Instructions unclear.')
 #imports
 import FileReader
 
 #main method
 def main():
-    print("Hello world")
+    display_interface()
     #FileReader.PrintPeopleDetails("test.txt")
     #FileReader.PrintEmployees()
+
+def display_interface():
+    path = input('Please enter path to files:\n')
+
+    sel = ''
+    while sel != '0':
+        print('\n1 - View file contents')
+        print('2 - View employees from files')
+        print('0 - Exit')
+
+        sel = input('Select an option: ')
+        if sel == '1':
+            print('1 selected')
+            FileReader.PrintPeopleDetails(path)
+        elif sel == '2':
+            print('2 selected')
+            FileReader.PrintEmployees(path)
+        elif sel == '0':
+            print('Closing program.')
+        else:
+            print('Instructions unclear.')
+
 
 
 if __name__ == "__main__":
