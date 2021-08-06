be33112518caf45d37fe22cb0be9519acae59291
JRosadoDiaz <jose.rosado853@gmail.com>
2021-08-05 18:53:06 -0600

    Added functionality to some methods
    + added a filepath global variable directing towards /people/long
    ~ modified method parameters to remove the use of a filepath in favor of hard coded path
    + AddEmployee has been added
    + UpdateEmployee has been added
    + DeleteEmployees has been added

--- FileReader.py
+++ FileReader.py
@@ -4,38 +4,50 @@ from Employee import Employee
 
 
 employees = []
+filepath = "./people/long/"
 
 def PrintPeopleDetails():
-    filepath = './people/long'
     for file in os.listdir("./Samples"):
-        f = open("./Samples/" + file, "r")
+        f = open(filepath + file, "r")
         print("FILE: " + file)
         print(f.read())
         print()
 
 def PrintEmployees():
-    read_files('./people/long')
+    read_files(filepath)
     for x in employees:
         x.toString()
 
 def AddEmployee(id, firstName, lastName, hireDate):
-    with open("./people/long" + id + ".txt", "w") as f:
-        f.write(id + ", " + firstName.upper() + ", " + lastName.upper() + ", " + hireDate)
+    #create a file
+    f = open(filepath + id + ".txt", 'access_mode')
 
-def DeleteEmployee(id):
-    os.remove("./people/long/" + id + ".txt")
+    #write parameter values to file
+    f.write(id + "," + firstName + "," + lastName + "," + hireDate)
+
+    #close file
+    f.close()
 
 def UpdateEmployee(id, firstName, lastName, hireDate):
-    with open("./people/long/" + id + ".txt", "w") as f:
-        f.write(id + ", " + firstName.upper() + ", " + lastName.upper() + ", " + hireDate)
+    #locate file with id
+    f = open(filepath + "/" + id + ".txt", "w")
 
-def read_files():
-    filepath = './people/long'
+    #rewrite contents of file
+    f.write(id + "," + firstName + "," + lastName + "," + hireDate)
+
+    #close file
+    f.close()
+
+def DeleteEmployees(id):
+    os.remove(filepath + "/" + id + ".txt")
+
+def read_files(filepath):
     for file in os.listdir(filepath):
         f = open(filepath + "/" + file, "r")
         lines = f.readlines()
         for x in lines:
             emp = x.split(",")
+            #print(emp)
             newEmployee = Employee(emp[0], emp[1], emp[2], emp[3])
             employees.append(newEmployee)
 
@@ -49,3 +61,18 @@ def check_id_exists(filepath, id):
 def SerializeAllEmployees(path, serPath):
     print("Serializing all employees...")
 
+
+
+# def get_next_id(filepath):
+#     thisNum = 0
+#     lastNum = 0
+
+    # doesn't work with default ascii sorting.
+
+    # for file in os.listdir(filepath):
+        # thisNum = int(file.split(".")[0])
+        # print(int(file.split('.')[0]))
+        # if (thisNum - 1 != lastNum):
+        #     return thisNum - 1
+        # lastNum = thisNum
+    #return thisNum + 1
