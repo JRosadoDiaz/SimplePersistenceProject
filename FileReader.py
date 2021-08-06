e2b729516ed2d319ab4a26ea663534db68cebe07
JRosadoDiaz <jose.rosado853@gmail.com>
2021-08-05 22:24:54 -0600

    Fixed some issues with previous CRUD operations and finished serialize methods
    FileReader.py
    ~ changed filepath variable to fix some issues with file pathing
    + added an unused method change_file_path for future use
    ~ fixed various filepath variables across the different methods
    ~ Fixed method name in DeleteEmployee
    + SerializeAllEmployees now serializes all txt files and moves them to the proper folder
    + GetSerializedEmployee now deserializes a given file and displays its information to the console
    ~ Fixed an issue in read_files where newEmployee was not being initialized before loop
    
    main.py
    ~ included call to unused method in FileReader.py, change_file_path, for future use
    ~ commented out console input to enter a path in option 6
    ~ view_selected_employee now points towards correct folder and has an updated input text
    + view_selected_employee now calls GetSerializedEmployee

--- FileReader.py
+++ FileReader.py
@@ -4,11 +4,15 @@ from Employee import Employee
 
 
 employees = []
-filepath = "./people/long/"
+filepath = "./people/long"
+
+# Not used yet
+def change_file_path(path):
+    filepath = path;
 
 def PrintPeopleDetails():
     for file in os.listdir(filepath):
-        f = open(filepath + file, "r")
+        f = open(filepath + "/" + file, "r")
         print("FILE: " + file)
         print(f.read())
         print()
@@ -20,7 +24,7 @@ def PrintEmployees():
 
 def AddEmployee(id, firstName, lastName, hireDate):
     #create a file
-    f = open(filepath + id + ".txt", 'w')
+    f = open(filepath + "/" + id + ".txt", 'w')
 
     #write parameter values to file
     f.write(id + "," + firstName + "," + lastName + "," + hireDate)
@@ -38,30 +42,47 @@ def UpdateEmployee(id, firstName, lastName, hireDate):
     #close file
     f.close()
 
-def DeleteEmployees(id):
+def DeleteEmployee(id):
     os.remove(filepath + "/" + id + ".txt")
 
-def SerializeAllEmployees(path, serPath):
+def SerializeAllEmployees():
     print("Serializing all employees...")
     # iterate through each file in people/long
     for file in os.listdir(filepath):
-        serFile = open(filepath + "/" + file, "ab") # ab = appending in binary format
-        # use the pickle library to serlialize each file into 'people/long serialized'
-        pickle.dump(serFile, filepath + " serialized")
-        serFile.close()
+        newEmployee = None
+        # read the file and generate an Employee object
+        f = open(filepath + "/" + file, 'r')
+        lines = f.readlines()
+        for x in lines:
+            emp = x.split(",")
+            newEmployee = Employee(emp[0], emp[1], emp[2], emp[3])
+        # Create the new serialized file
+        serFilePath = filepath + " serialized/" + newEmployee.get_employee_id()
+        fser = open(serFilePath + ".ser", 'ab')
+        pickle.dump(newEmployee, fser)
+        fser.close()
+
+    print("Done!")
 
 def GetSerializedEmployee(id):
-    # go to serialized folder and 
+    print("Searching for employee with id " + str(id))
+    # go to serialized folder and iterate over each file
+    for file in os.listdir(filepath + " serialized"):
+        # once found, deserialize and return employee object too string
+        if file == str(id) + ".ser":
+            fileSer = open(filepath + " serialized/" + file, "rb")
+            file = pickle.load(fileSer)
+            print(file.toString())
 
 def read_files(filepath):
     for file in os.listdir(filepath):
         f = open(filepath + "/" + file, "r")
         lines = f.readlines()
+        newEmployee = None
         for x in lines:
             emp = x.split(",")
-            #print(emp)
             newEmployee = Employee(emp[0], emp[1], emp[2], emp[3])
-            employees.append(newEmployee)
+        employees.append(newEmployee)
 
 def check_id_exists(filepath, id):
     for file in os.listdir(filepath):
